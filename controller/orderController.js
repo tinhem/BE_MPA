c45d7b77fde1f489ab3bc7f2ccd4a651417f41fd
tinhem <20020341@vnu.edu.vn>
2023-12-28 16:13:10 +0700

    update statu order

--- controller/orderController.js
+++ controller/orderController.js
@@ -11,6 +11,29 @@ const createOrder = asyncHandler(async (req, res) => {
   }
 });
 
+
+const updateOrderStatus = asyncHandler(async (req, res, next) => {
+  const { orderId, orderStatus } = req.body; // có orderId và orderStatus từ req.body
+  validateMongoDbId(orderId);
+
+  try {
+    const order = await Order.findById(orderId); 
+    if (!order) {
+      return res.status(404).json({ message: "Order not found" });
+    }
+
+    // Cập nhật trạng thái đơn hàng
+    order.orderStatus = orderStatus;
+
+    const updatedOrder = await order.save();
+    res.json(updatedOrder);
+  } catch (err) {
+    // Xử lý lỗi và trả về phản hồi lỗi
+    console.error(err);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
 const getOrder = asyncHandler(async (req, res) => {
   const { id } = req.params;
   validateMongoDbId(id);
@@ -33,6 +56,7 @@ const getAllOrder = asyncHandler(async (req, res) => {
 
 module.exports = {
   createOrder,
+  updateOrderStatus,
   getOrder,
   getAllOrder,
 };
